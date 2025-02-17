4cb5e873ded7a373e188164761f9ed77c7f57584
¨dim50dim¨ <zheliezkov@gmail.com>
2025-02-17 06:11:40 +0100

    in this task we touched filter method

--- unit_17.js
+++ unit_17.js
@@ -64,7 +64,7 @@ document.querySelector('.b-3').onclick = () => {
 let a4 = [4, "3", 6, 7, "12", 34, "56", 78, 90, 11];
 
 function t4() {
-
+       return a4_res = a4.filter(item => typeof item === 'number');
 }
 
 document.querySelector('.b-4').onclick = () => {
